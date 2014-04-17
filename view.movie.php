61898ed101a8bb9b2f6e7963d53eb9f7bdea44c6
Fábio Rocha <fabiorochafg@gmail.com>
2014-04-17 17:39:48 -0300

    Fixing the masonry plugin problem.

--- view.movie.php
+++ view.movie.php
@@ -5,42 +5,34 @@
 <? include "inc.head.php"; ?>
 <script src="files/jquery.masonry.min.js"></script>
 <script>
+function startMasonry() {
+    var container = document.querySelector('#boxes');
+    var msnry = new Masonry( container, {
+        columnWidth: 45,
+        itemSelector: '.box-item'
+    });
+}
 $(function() {
-	var $container = $('#boxes');
-	// var msnry = $container.data('masonry');
-	$container.masonry({
-		itemSelector: '.box-item',
-		isReasizable: true
-	});
-    $(".tabs a").click(function() {
-    	var ano = $(this).attr("data-year");
-    	$(".tabs a").removeClass("active");
-    	$(this).addClass("active");
-    	if (ano == "Todos") {
-    		$('.box-item').show();
-    	} else {
-	        $('.box-item').each(function() {
-	            if ($(this).attr("data-year") == ano) {
-	                $(this).show();
-	            } else {
-	            	$(this).hide();
-	            }
-	        });
-	        $container.masonry({
-		itemSelector: '.box-item',
-		isReasizable: true
-	});   		
-    	}
-        // msnry.reloadItems();
-        // $container.masonry('reloadItems');
-        // $container.masonry('reveal', itemsAll);
-        // msnry.hide(itemsOff);
-        // $container.masonry('hide', itemsOff);
-  		// $('#boxes').masonry({
-		// itemSelector: '.box-item',
-		// 	isReasizable: true
-		// });
-        return false;
+
+    startMasonry();
+    
+    $(".tabs a").on('click',function (e) {
+        e.preventDefault();
+        var ano = $(this).attr("data-year");
+        $(".tabs a").removeClass("active");
+        $(this).addClass("active");
+        if (ano == "Todos") {
+            $('.box-item').show();
+        } else {
+            $('.box-item').each(function() {
+                if ($(this).attr("data-year") == ano) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+        startMasonry();
     });
 });
 </script>
@@ -49,50 +41,50 @@ $(function() {
 <body>
 <section>
     <? include "inc.top.php"; ?>
-	<?php
-	// Visualizar itens
-	$tipo = $_GET['tipo'];
-	switch ($tipo) {
-		case 0: $filme = "DVD"; break;
-		case 1: $filme = "Blu-ray"; break;
-	}
-	$consulta = mysql_query('SELECT titulo,diretor,ano,especificacoes,preco,imagem,id_filme
-		FROM filme
-		WHERE tipo='.$tipo.'
-		ORDER BY ano DESC');
-	$n = mysql_num_rows($consulta);
-	echo '<div id="content">
-	<h2>Lista de '.$filme.'s ('.$n.' itens)</h2>';	
-	if (isset($_GET['msg'])) {
-		switch ($_GET['msg']) {
-			case 1: echo "<p class='msg success'>Filme atualizado com sucesso!</p>"; break;
-			case 2: echo "<p class='msg error'>Não foi possível atualizar este filme.</p>"; break;
-		}
-	}
-	$consultaAno = mysql_query('SELECT ano FROM filme WHERE tipo='.$tipo.' GROUP BY ano ORDER BY ano DESC');
-	echo '<ul class="tabs">';
-	while ($dadosAno = mysql_fetch_row($consultaAno)) {
-		echo '<li><a href="#" data-year="'.$dadosAno[0].'">'.$dadosAno[0].'</a></li>';
-	}
-	echo '<li><a href="#" data-year="Todos" class="active">Todos</a></li>
-	</ul>
-	</div>
-	<div id="boxes">';
-		while ($dados = mysql_fetch_row($consulta)) {
-			echo "<div class='box-item' data-year='".$dados[2]."'>
-				<h3>".$dados[0]."</h3>
-				<h4>".$dados[2]."</h4>
-				<div>
-					<img src='photos/".$dados[5]."' alt='".$dados[0]."' />
-					<ul>
-						<li><a href='edit.movie.php?cod=1&id='".$dados[6]."'>Editar</a></li>
-						<li><a href='edit.movie.php?cod=3&id='".$dados[6]."' class='danger'>Excluir</a></li>
-					</ul>
-				</div>
-			</div>";
-		}
-	echo '</div>';
-	?>    
+    <?php
+    // Visualizar itens
+    $tipo = $_GET['tipo'];
+    switch ($tipo) {
+        case 0: $filme = "DVD"; break;
+        case 1: $filme = "Blu-ray"; break;
+    }
+    $consulta = mysql_query('SELECT titulo,diretor,ano,especificacoes,preco,imagem,id_filme
+        FROM filme
+        WHERE tipo='.$tipo.'
+        ORDER BY ano DESC');
+    $n = mysql_num_rows($consulta);
+    echo '<div id="content">
+    <h2>Lista de '.$filme.'s ('.$n.' itens)</h2>';  
+    if (isset($_GET['msg'])) {
+        switch ($_GET['msg']) {
+            case 1: echo "<p class='msg success'>Filme atualizado com sucesso!</p>"; break;
+            case 2: echo "<p class='msg error'>Não foi possível atualizar este filme.</p>"; break;
+        }
+    }
+    $consultaAno = mysql_query('SELECT ano FROM filme WHERE tipo='.$tipo.' GROUP BY ano ORDER BY ano DESC');
+    echo '<ul class="tabs">';
+    while ($dadosAno = mysql_fetch_row($consultaAno)) {
+        echo '<li><a href="#" data-year="'.$dadosAno[0].'">'.$dadosAno[0].'</a></li>';
+    }
+    echo '<li><a href="#" data-year="Todos" class="active">Todos</a></li>
+    </ul>
+    </div>
+    <div id="boxes" class="js-masonry">';
+        while ($dados = mysql_fetch_row($consulta)) {
+            echo "<div class='box-item' data-year='".$dados[2]."'>
+                <h3>".$dados[0]."</h3>
+                <h4>".$dados[2]."</h4>
+                <div>
+                    <img src='photos/".$dados[5]."' alt='".$dados[0]."' />
+                    <ul>
+                        <li><a href='edit.movie.php?cod=1&id='".$dados[6]."'>Editar</a></li>
+                        <li><a href='edit.movie.php?cod=3&id='".$dados[6]."' class='danger'>Excluir</a></li>
+                    </ul>
+                </div>
+            </div>";
+        }
+    echo '</div>';
+    ?>    
 </section>
 <? include "inc.nav.php"; ?>
 <? include "inc.footer.php"; ?>
